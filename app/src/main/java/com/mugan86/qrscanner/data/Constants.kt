6cb3a2d13b3364a5f8021f5bc8524757e8a39094
Anartz Mugika <mugan86@gmail.com>
2018-07-13 19:11:44 +0200

    Add constants

---
+++ app/src/main/java/com/mugan86/qrscanner/data/Constants.kt
@@ -0,0 +1,22 @@
+package com.mugan86.qrscanner.data
+
+import com.mugan86.qrscanner.R
+
+/********************************************************************************
+ * Created by Anartz Mugika (mugan86@gmail.com) on 13/7/18.
+ ********************************************************************************/
+class Constants {
+
+    companion object{
+
+        const val BAR_CODE = "BarCode"
+        const val BAR_CODE_NOT_FOUND = "Bar code not found"
+        const val ERROR = "Error"
+        const val HANDLER = "handler"
+        const val PRESS_AGAIN_TO_SCAN = "Press again button to scan code"
+        const val SCAN_ERROR_SOUND = R.raw.computer_error
+        const val SCAN_OK_SOUND = R.raw.beep_normal
+        const val STOP_CAMERA = "************** Stop Camera**********"
+
+    }
+}
